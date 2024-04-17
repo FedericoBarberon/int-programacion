e8d9d3b489906b657678a90ad37c09fff340944f
Federico Barberon <jfedericobarberonj@gmail.com>
2024-04-17 15:56:57 -0300

    añade ejercicios del 10 al 13

--- guia4.hs
+++ guia4.hs
@@ -24,7 +24,7 @@ problema esDivisible(a: Z, b: Z): Bool {
 
 esDivisible :: Integer -> Integer -> Bool
 esDivisible a b
-    | a >= b = esDivisible (a-b) b
+    | a >= b = esDivisible (a - b) b
     | otherwise = a == 0
 
 -- MARK: Ejercicio 4
@@ -44,18 +44,18 @@ problema sumaImpares(n: Z): Z {
 sumaImparesHasta :: Integer -> Integer
 sumaImparesHasta n
     | n <= 0 = 0
-    | mod n 2 /= 0 = n + sumaImparesHasta (n-1)
-    | otherwise = 0 + sumaImparesHasta (n-1)
+    | mod n 2 /= 0 = n + sumaImparesHasta (n - 1)
+    | otherwise = 0 + sumaImparesHasta (n - 1)
 
 sumaImpares :: Integer -> Integer
-sumaImpares n = sumaImparesHasta (n*2 - 1)
+sumaImpares n = sumaImparesHasta (n * 2 - 1)
 
 -- MARK: Ejercicio 5
 
 medioFact :: Integer -> Integer
 medioFact 0 = 1
 medioFact 1 = 1
-medioFact n = n * medioFact (n-2)
+medioFact n = n * medioFact (n - 2)
 
 -- MARK: Ejercicio 6
 
@@ -97,11 +97,117 @@ problema esCapicua(n: Z): Bool {
 }
 -}
 
-esCapicua_aux :: Integer -> Integer -> Bool
-esCapicua_aux n i
-    | div (nDigitos) 2 >= i = iesimoDigito n i == iesimoDigito n (nDigitos + 1 - i) && esCapicua_aux n (i+1)
+esCapicuaAux :: Integer -> Integer -> Bool
+esCapicuaAux n i
+    | div nDigitos 2 >= i = iesimoDigito n i == iesimoDigito n (nDigitos + 1 - i) && esCapicuaAux n (i + 1)
     | otherwise = True
-    where nDigitos = cantDigitos n
+  where
+    nDigitos = cantDigitos n
 
 esCapicua :: Integer -> Bool
-esCapicua n = esCapicua_aux n 1
+esCapicua n = esCapicuaAux n 1
+
+-- MARK: Ejercicio 10
+
+{- a)
+problema f1(n: Z): Z {
+    requiere: {n >= 0}
+    asegura: {res = la sumatoria de i en 0 hasta n de n^i}
+}
+-}
+
+f1 :: Integer -> Integer
+f1 0 = 1
+f1 n = f1 (n - 1) + 2 ^ n
+
+{- b)
+problema f2(n: Z, q: R): R {
+    requiere: {n > 0}
+    asegura: {res = la sumatoria de i en 1 hasta n de q^i}
+}
+-}
+
+f2 :: Integer -> Float -> Float
+f2 1 q = q
+f2 n q = f2 (n - 1) q + q ^ n
+
+{- c)
+problema f3(n: Z, q: R): R {
+    requiere: {n >= 0}
+    asegura: {res = la sumatoria de i en 1 hasta 2n de q^i}
+}
+-}
+
+f3 :: Integer -> Float -> Float
+f3 0 _ = 0
+f3 n q = f2 (2 * n) q
+
+{- d)
+problema f4(n: Z, q: R): R {
+    requiere: {n >= 0}
+    asegura: {res = la sumatoria de i en n hasta 2n de q^i}
+}
+-}
+
+f4Aux :: Integer -> Float -> Float
+f4Aux 0 _ = 1
+f4Aux n q = f4Aux (n - 1) q + q ^ n
+
+f4 :: Integer -> Float -> Float
+f4 0 _ = 1
+f4 n q = f4Aux (2 * n) q - f4Aux (n - 1) q
+
+-- MARK: Ejercicio 11
+
+{- a)
+problema eAprox(n: Z): R {
+    requiere: {n >= 0}
+    asegura: {res = la sumatoria de i en 0 hasta n de 1/i!}
+}
+-}
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+eAprox :: Integer -> Float
+eAprox 0 = 1
+eAprox n = eAprox (n - 1) + 1 / (fromIntegral (factorial n))
+
+-- b)
+
+e :: Float
+e = eAprox 10
+
+-- MARK: Ejercicio 12
+
+{-
+problema raizDe2Aprox(n: Z): R {
+    requiere: {n > 0}
+    asegura: {res = an - 1, donde a1 = 2, an = 2 + 1/a(n-1) }
+}
+-}
+
+raizDe2AproxAux :: Integer -> Float
+raizDe2AproxAux 1 = 2
+raizDe2AproxAux n = 2 + 1 / (raizDe2AproxAux (n - 1))
+
+raizDe2Aprox :: Integer -> Float
+raizDe2Aprox n = raizDe2AproxAux n - 1
+
+-- MARK: Ejercicio 13
+
+{-
+problema fEj13(n: Z, m: Z): Z {
+    requiere: {n > 0 && m > 0}
+    asegura: {res = la sumatoria de i en 1 hasta n de la sumatoria de j en 1 hasta m de i^j}
+}
+-}
+
+fEj13Aux :: Integer -> Integer -> Integer
+fEj13Aux n 1 = n
+fEj13Aux n m = fEj13Aux n (m - 1) + n ^ m
+
+fEj13 :: Integer -> Integer -> Integer
+fEj13 1 m = fEj13Aux 1 m
+fEj13 n m = fEj13 (n - 1) m + fEj13Aux n m
