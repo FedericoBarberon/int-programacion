5a24a71c64fb591a6b6c423bf8f3837569b65ca1
Federico Barberon <jfedericobarberonj@gmail.com>
2024-06-01 01:39:24 -0300

    añade pruebas con la api de Google Translate

---
+++ pruebas-api/translator.py
@@ -0,0 +1,16 @@
+from googletrans import Translator
+
+def traducirTexto(nombre_archivo: str, idioma_origen: str, idioma_destino: str):
+    translator: Translator = Translator()
+
+    input_file = open(nombre_archivo, "r", encoding="utf-8")
+    contenido: str = input_file.read()
+    input_file.close()
+
+    contenido_traducido: str = translator.translate(contenido, src=idioma_origen, dest=idioma_destino).text
+
+    output_file = open(f"{idioma_destino}-{nombre_archivo.split('/')[-1]}", "w", encoding="utf-8")
+    output_file.write(contenido_traducido)
+    output_file.close()
+
+traducirTexto("texto.txt", "es", "zh-cn")
